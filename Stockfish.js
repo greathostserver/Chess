34f66b5edb814e715f47fe0873fe9baaa9ac40a1
greathostserver <mymailisme1967bbs@gmail.com>
2025-11-26 05:42:08 +0330

    Create Stockfish.js

---
+++ Stockfish.js
@@ -0,0 +1,7 @@
+// این فایل Stockfish.js است - برای نسخه کامل از لینک بالا استفاده کنید
+// فایل کامل از: https://cdnjs.cloudflare.com/ajax/libs/stockfish.js/10.0.2/stockfish.js
+
+// برای تست می‌توانید از این کد ساده استفاده کنید (البته نسخه کامل را توصیه می‌کنم):
+self.importScripts('https://cdnjs.cloudflare.com/ajax/libs/stockfish.js/10.0.2/stockfish.js');
+
+// یا اگر می‌خواهید فایل local داشته باشید، فایل کامل را دانلود کنید
